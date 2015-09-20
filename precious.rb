90ff7440cfce12415ade99d4e7e8aec08b73857d
Naoya Murakami <visionary.incident@gmail.com>
2015-09-20 18:03:25 +0900

    Fix auth

--- precious.rb
+++ precious.rb
@@ -4,13 +4,15 @@ class Precious::App
   basic_auth_username = ENV['BASIC_AUTH_USERNAME']
   basic_auth_password = ENV['BASIC_AUTH_PASSWORD']
   basic_auth_manage = ENV['BASIC_AUTH_MANAGE_ONLY']
-  if basic_auth_username && basic_auth_password && basic_auth_manage
-    use ManageAuth do |username, password|
-      [username, password] == [basic_auth_username, basic_auth_password]
-    end
-  else
-    use Rack::Auth::Basic, "Restricted Area" do |username, password|
-      [username, password] == [basic_auth_username, basic_auth_password]
+  if basic_auth_username && basic_auth_password
+    if basic_auth_manage
+      use ManageAuth do |username, password|
+        [username, password] == [basic_auth_username, basic_auth_password]
+      end
+    else
+      use Rack::Auth::Basic, "Restricted Area" do |username, password|
+        [username, password] == [basic_auth_username, basic_auth_password]
+      end
     end
   end
 end
